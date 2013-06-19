907cc013529647841b34c541fcb836f20a294955
pandazheng <543828315@qq.com>
2013-06-19 09:18:42 -0700

    SQLiteDemo
    SQLiteDemo

--- SQLiteDemo/ViewController.h
+++ SQLiteDemo/ViewController.h
@@ -7,7 +7,27 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <sqlite3.h>
 
 @interface ViewController : UIViewController
+{
+    sqlite3 *contactDB;
+    
+    UITextField *name;
+    UITextField *address;
+    UITextField *phone;
+    
+    UILabel *status;
+    NSString *databasePath;
+}
+
+@property (retain,nonatomic) IBOutlet UITextField *name;
+@property (retain,nonatomic) IBOutlet UITextField *address;
+@property (retain,nonatomic) IBOutlet UITextField *phone;
+@property (retain,nonatomic) IBOutlet UILabel *status;
+
+- (IBAction) SaveToDataBase :(id)sender;
+- (IBAction) SearchFromDataBase :(id)sender;
+- (IBAction) backgroundTap:(id)sender;
 
 @end
